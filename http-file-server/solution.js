3280ff078abd45219f14f3d2f3fa825bf4b9393d
Gustavo <gfasequeira@gmail.com>
2015-05-06 00:19:17 -0300

    http file server

---
+++ http-file-server/solution.js
@@ -0,0 +1,8 @@
+var fs = require('fs');
+var http = require('http');
+var server = http.createServer(function (request, response) {
+	response.writeHead(200, {'content-type': 'text/plain'})
+	
+	fs.createReadStream(process.argv[3]).pipe(response)
+})
+server.listen(Number(process.argv[2]))
